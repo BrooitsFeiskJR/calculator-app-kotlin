6faa1cd01828196e49194d83398a3cfa818c58ce
Luiz Antonio <luizantonio0125@gmail.com>
2023-02-27 13:06:38 -0300

    Using OnClick and Implementing the CLR Functionality

--- app/src/main/java/br/toninho/calculator_app/MainActivity.kt
+++ app/src/main/java/br/toninho/calculator_app/MainActivity.kt
@@ -3,15 +3,24 @@ package br.toninho.calculator_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private var tvInput: TextView? = null;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
     fun onDigit(view: View) {
-        Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
+        tvInput = findViewById(R.id.tvInput)
+        tvInput?.append((view as Button).text)
+    }
+
+    fun onClear(view: View) {
+        tvInput?.text = ""
     }
 }
